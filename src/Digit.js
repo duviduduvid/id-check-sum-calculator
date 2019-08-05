db302737e1ccb46c1e33441e34cafd712037f602
David Franco <duviduduvid@gmail.com>
2019-08-05 11:44:38 +0300

    more auto code formatting

--- src/Digit.js
+++ src/Digit.js
@@ -3,33 +3,34 @@ import { Input } from 'semantic-ui-react';
 import './Digit.css';
 
 const Digit = props => {
-
   const inputElement = useRef(null);
 
   const handleDigitChange = event => {
     const input = event.target.value;
-    let value = input && parseInt(input, 10); 
+    let value = input && parseInt(input, 10);
     props.updateDigit(props.digit.index, validateAndNormalize(value));
   };
 
-  const validateAndNormalize = value => (  
-    Number.isNaN(value) || value === '' ?  '' :
-      (value >= 0 && value <= 9) ? Math.floor(value) : Math.abs(value % 10)
-  );
+  const validateAndNormalize = value =>
+    Number.isNaN(value) || value === ''
+      ? ''
+      : value >= 0 && value <= 9
+      ? Math.floor(value)
+      : Math.abs(value % 10);
 
   useEffect(() => {
-    if (props.digit.isFocused) {
+    if (props.isFocused) {
       inputElement.current.focus();
     }
   });
 
   return (
-      <Input 
-        ref={inputElement}
-        value={props.digit.value} 
-        onChange={handleDigitChange}
-      />
+    <Input
+      ref={inputElement}
+      value={props.digit.value}
+      onChange={handleDigitChange}
+    />
   );
-}
+};
 
 export default Digit;
